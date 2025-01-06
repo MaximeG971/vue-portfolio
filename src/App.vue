8e8cd8366efac662a930617b3e2ea0aa505957ea
MAXIME GAEL <maximejordan.gael@gmail.com>
2025-01-06 16:06:34 +0100

    add the back to the top component and LoadingSpinner

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="bg-[#111827] min-h-screen">
-    <NavBar />
-    <IntroductionSection />
-    <ExperienceAndSkills />
-    <LatestProjectsSection />
-    <ContactSection />
-    <Footer />
+    <!-- Wrap components in Suspense to handle loading -->
+    <Suspense>
+      <template #default>
+        <!-- Wrap all Components inside a single root element -->
+        <div>
+          <NavBar />
+          <IntroductionSection />
+          <ExperienceAndSkills />
+          <LatestProjectsSection />
+          <ContactSection />
+          <Footer />
+          <BackToTop />
+        </div>
+      </template>
+      <template #fallback>
+        <div class="flex justify-center items-center min-h-screen">
+          <LoadingSpinner />
+        </div>
+      </template>
+    </Suspense>
   </div>
 </template>
 
@@ -27,4 +41,10 @@ const ContactSection = defineAsyncComponent(() =>
   import("@/components/ContactSection.vue")
 );
 const Footer = defineAsyncComponent(() => import("@/components/Footer.vue"));
+const BackToTop = defineAsyncComponent(() =>
+  import("@/components/BackToTop.vue")
+);
+const LoadingSpinner = defineAsyncComponent(() =>
+  import("@/components/LoadingSpinner.vue")
+);
 </script>
