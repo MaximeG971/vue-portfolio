5c7b5093caf234e2f14e958514cebbdc2d16a104
MAXIME GAEL <maximejordan.gael@gmail.com>
2025-01-06 16:10:44 +0100

    add my logo and update the name + delete unused anchors

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -56,11 +56,8 @@
 <script setup>
 import { ref } from "vue";
 const Menu = ref([
-  { name: "Services", href: "#services" },
-  { name: "About Me", href: "#about" },
   { name: "Skills", href: "#skills" },
   { name: "Projects", href: "#projects" },
-  { name: "Testimonials", href: "#testimonials" },
   { name: "Contact", href: "#contact" },
 ]);
 
