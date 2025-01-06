3b5d0a7a2386c88006719317df9ebf2aef87e6fa
MAXIME GAEL <maximejordan.gael@gmail.com>
2025-01-06 15:37:02 +0100

    my vue portfolio

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="bg-[#111827] min-h-screen">
+    <NavBar />
+    <IntroductionSection />
+    <ExperienceAndSkills />
+    <LatestProjectsSection />
+    <ContactSection />
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import { defineAsyncComponent } from "vue";
+// Import Components Asynchronously
+
+const NavBar = defineAsyncComponent(() => import("@/components/NavBar.vue"));
+const IntroductionSection = defineAsyncComponent(() =>
+  import("@/components/IntroductionSection.vue")
+);
+const ExperienceAndSkills = defineAsyncComponent(() =>
+  import("@/components/ExperienceAndSkills.vue")
+);
+const LatestProjectsSection = defineAsyncComponent(() =>
+  import("@/components/LatestProjectsSection.vue")
+);
+const ContactSection = defineAsyncComponent(() =>
+  import("@/components/ContactSection.vue")
+);
+const Footer = defineAsyncComponent(() => import("@/components/Footer.vue"));
+</script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer
+    class="mt-8 border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white"
+  >
+    <div class="container p-12 flex justify-between">
+      <div>
+        <img src="@/assets/MG-logo.png" alt="logo" class="w-20" />
+      </div>
+      <p class="text-slate-600">All rights reserved.</p>
+    </div>
+  </footer>
+</template>
